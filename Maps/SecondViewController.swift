2f0ca54fbdc8e2b6b5ea11c53901c4934a8df265
Sebastian Osiński <seb.osinski@gmail.com>
2019-08-22 18:40:59 +0200

    Initial commit

---
+++ Maps/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  Maps
+//
+//  Created by Sebastian Osiński on 22/08/2019.
+//  Copyright © 2019 Sebastian Osiński. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
